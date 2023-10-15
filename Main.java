c04b8be8c28f86a4a6b55cd97f1a0291d32740e5
user <user@WIN-MHGL1PRC8MH>
2023-10-15 16:16:39 +0300

    1

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileReader;
@@ -7,15 +6,36 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Ex: ");
+
+        try (Scanner in = new Scanner(System.in)) {
+            int a = in.nextInt();
+
+            switch (a) {
+                case 1:
+                    main1(args);
+                    break;
+
+                case 2:
+                    main2(args);
+
+            }
+        }
+    }
+
+    public static void main1(String[] args) throws IOException {
 
         int n = 0;
         try (Scanner in = new Scanner(System.in)) {
             System.out.print("File: ");
             String fileName = in.next();
+            System.out.print("File1: ");
+            String fileName1 = in.next();
 
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
@@ -96,17 +116,48 @@ public class Main {
             System.out.println("\nFinall: ");
 
             for (int i = 0; i < ia.length; i++) {
-                if (ia[i] < sa) {
-                    System.out.println(ia[i]);
+                if (ia[i] >= sa) {
+                    System.out.println(ia[i++]);
 
                 } else {
-                    FileWriter fw = new FileWriter(fileName, true);
+                    FileWriter fw = new FileWriter(fileName1, true);
                     BufferedWriter bw = new BufferedWriter(fw);
                     bw.write("\n\n\n" + ia[i]);
                     bw.newLine();
                     bw.close();
                 }
             }
+        }
+    }
+
+    public static void main2(String[] args) throws IOException {
+
+        File file = new File("N1.txt");
+        File file1 = new File("N2.txt");
+        File file2 = new File("N3.txt");
+        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
+        try (Scanner sc = new Scanner(new FileInputStream(file))) {
+            while (sc.hasNext()) {
+                sc.next();
+                count++;
+            }
+            System.out.println("File: " + file + "  |  Number: " + count);
+        }
+        try (Scanner sc = new Scanner(new FileInputStream(file1))) {
+            while (sc.hasNext()) {
+                sc.next();
+                count1++;
+            }
+            System.out.println("File: " + file1 + "  |  Number: " + count1);
+        }
+        try (Scanner sc = new Scanner(new FileInputStream(file2))) {
+            while (sc.hasNext()) {
+                sc.next();
+                count2++;
+            }
+            System.out.println("File: " + file2 + "  |  Number: " + count2 + "\n");
         }
     }
 }
